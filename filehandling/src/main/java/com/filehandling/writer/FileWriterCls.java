0b870b5cf483a5d7306cb2aabcdacab60aa50775
vaibhavkhot1709 <vaibhavkhot1617gmail.com>
2023-01-05 19:00:50 +0530

    firtddd

--- filehandling/src/main/java/com/filehandling/writer/FileWriterCls.java
+++ filehandling/src/main/java/com/filehandling/writer/FileWriterCls.java
@@ -1,5 +1,6 @@
 package com.filehandling.writer;
 
+import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -9,12 +10,20 @@ public class FileWriterCls {
 		System.out.println("main started");
 		FileWriter fr = new FileWriter("D:/JAVA/FileHandling/FileWrite/filerwiting.txt");
 
-		fr.write("my name is vaibhav khot");
+//		fr.write("my name is vaibhav khot");
 //		fr.write(" im from kolhapur district of MH");
+		
+		
+		System.out.println("to add multiple lines at time we use BuferWriter class");
+		
+		BufferedWriter br=new BufferedWriter(fr);
+		br.write("my name is vaibhav khot");
+		
+		br.close();
 		fr.close();
 		
-		System.out.println("succ");
 
+		System.out.println("filw writing successfullt donesucc");
 	}
 
 }
