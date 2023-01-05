6e1c0058903aa8b16db8ff29e17d6d407519dfa7
vaibhavkhot1709 <vaibhavkhot1617gmail.com>
2023-01-05 18:57:32 +0530

    demo

--- filehandling/src/main/java/com/filehandling/writer/FileWriterCls.java
+++ filehandling/src/main/java/com/filehandling/writer/FileWriterCls.java
@@ -1,9 +1,20 @@
 package com.filehandling.writer;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class FileWriterCls {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+
+		System.out.println("main started");
+		FileWriter fr = new FileWriter("D:/JAVA/FileHandling/FileWrite/filerwiting.txt");
+
+		fr.write("my name is vaibhav khot");
+//		fr.write(" im from kolhapur district of MH");
+		fr.close();
 		
-		System.out.println("main");
+		System.out.println("succ");
+
 	}
 
 }
